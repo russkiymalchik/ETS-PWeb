eb4afb3af352277ab2a2a8a2e06a15a87c497811
russkiymalchik <khairywiryawan01@gmail.com>
2021-10-30 10:42:33 +0700

    new insertion admin

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,10 +19,29 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->integer('is_admin')->default(1);
+            $table->integer('is_admin')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
+
+        //insert new data
+        DB::table('users')->insert(
+            array(
+                'name' => 'Admin',
+                'email' => 'admin@gmail.com',
+                'password' => bcrypt('password'),
+                'is_admin' => 1
+            )
+        );
+
+        DB::table('users')->insert(
+            array(
+                'name' => 'Admin1',
+                'email' => 'admin1@gmail.com',
+                'password' => bcrypt('password1'),
+                'is_admin' => 1
+            )
+        );
     }
 
     /**
